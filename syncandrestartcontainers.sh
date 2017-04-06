262c65b66691b52f0dd21340fbeb54974dad9303
BrandonFox <bfox@foxtrot.email>
2017-04-06 18:30:47 -0500

    delete

--- syncandrestartcontainers.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-eval $(ssh-agent)
-ssh-add ~/.docker/machine/machines/default/id_rsa
-rsync -avzhe ssh --relative --omit-dir-times --progress ./ docker@$(docker-machine ip default):$(pwd)
-docker-compose -f dev.yml up
