156c44f13950d30b97b6e7c49e68f2cb160b5d42
Brandon Fox <bfox@foxtrot.email>
2017-04-21 19:11:32 -0400

    failing test

--- nectr/search/tests.py
+++ nectr/search/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class SearchTests(TestCase):
+    def test_can_receive_a_POST(self):
+        response = self.client.post('/search', data={'search_text': 'Computer Science'})
+        self.assertIn('Computer Science', response.content.decode())
