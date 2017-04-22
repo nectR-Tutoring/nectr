89376274719f8107ce3a5dd9409c36a0f712ae74
Brandon Fox <bfox@foxtrot.email>
2017-04-21 20:08:52 -0400

    failing test for search page 404

--- nectr/search/tests.py
+++ nectr/search/tests.py
@@ -1,8 +1,20 @@
 from django.test import TestCase
 
-
 # Create your tests here.
+from django.urls import resolve
+
+from nectr.search.views import search_page
+
+
 class SearchTests(TestCase):
-    def test_can_receive_a_POST(self):
-        response = self.client.post('/search', data={'search_text': 'Computer Science'})
-        self.assertIn('Computer Science', response.content.decode())
+    def test_root_url_resolves_to_search_page_view(self):
+        found = resolve('search/')
+        self.assertEqual(found.func, search_page)
+
+    # def test_uses_search_template(self):
+    #     response = self.client.get('search/')
+    #     self.assertTemplateUsed(response, 'search.html')
+
+    # def test_can_receive_a_POST(self):
+    #     response = self.client.post('search/', data={'search_text': 'Computer Science'})
+    #     self.assertIn('Computer Science', response.content.decode())
--- nectr/search/views.py
+++ nectr/search/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def search_page():
+    pass
+
+
+def search(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['Computer Science'])
