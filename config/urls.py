d173aa823bb3b71d57abfeb07e36d08da296d757
Charlie Valerio <valec4@farmingdale.edu>
2017-04-26 16:03:25 -0400

    Merge branch 'pagelink_nectr_base' into new_development
    # Conflicts:
    #	config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -9,8 +9,8 @@ from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
-                  url(r'^$', TemplateView.as_view(template_name='pages/index.html'), name='home'),
-                  url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+                  url(r'^$', TemplateView.as_view(template_name='homepage.html'), name='home'),
+                  url(r'^about/$', TemplateView.as_view(template_name='about_nectr.html'), name='about'),
 
                   # Django Admin, use {% url 'admin:index' %}
                   url(settings.ADMIN_URL, admin.site.urls),
@@ -24,14 +24,15 @@ urlpatterns = [
 
 
     # Search the Hive
-    url(r'^search_the_hive', TemplateView.as_view(template_name='pages/search_the_hive.html'), name='search'),
+    url(r'^search_the_hive', TemplateView.as_view(template_name='look_nectr.html'), name='search'),
     # Join the Hive
-    url(r'^join_the_hive', TemplateView.as_view(template_name='pages/join_the_hive.html'), name='join'),
-    # About the Hive
-    url(r'^about', TemplateView.as_view(template_name='pages/about.html'), name='about'),
-    # How it Works
-    url(r'^how_it_works', TemplateView.as_view(template_name='pages/how_it_works.html'), name='how_it_works'),
+    url(r'^join_the_hive', TemplateView.as_view(template_name='joinpage_nectr.html'), name='join'),
 
+    # How it Works
+    url(r'^how_it_works', TemplateView.as_view(template_name='how_nectr.html'), name='how_it_works'),
+
+
+<<<<<<< HEAD
 <<<<<<< HEAD
     url(r'^test_index_html', TemplateView.as_view(template_name='homepage.html'), name='test1'),
     url(r'^test_about_nectr', TemplateView.as_view(template_name='about_nectr.html'), name='test2'),
@@ -45,6 +46,10 @@ urlpatterns = [
     url(r'^test_html', TemplateView.as_view(template_name='nectr_base.html'), name='test'),
     url(r'^test_index_html', TemplateView.as_view(template_name='index.html'), name='test1'),
 >>>>>>> origin/new_development
+=======
+    #url(r'^test_look_nectr', TemplateView.as_view(template_name='look_nectr.html'), name='test4'),
+    #url(r'^test_joinpage_nectr', TemplateView.as_view(template_name='joinpage_nectr.html'), name='test5'),
+>>>>>>> pagelink_nectr_base
 
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
