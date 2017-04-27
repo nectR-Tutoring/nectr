8696cd1a559b907ff187f5b0400a933f0984ffd8
Brandon Fox <bfox@foxtrot.email>
2017-04-26 20:43:14 -0400

    Build Tutor Views

--- nectr/student/models.py
+++ nectr/student/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from nectr.users.models import User
+
+
+class Student(models.Model):
+    user = models.ForeignKey(User)
--- nectr/tutor/models.py
+++ nectr/tutor/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+from nectr.student.models import Student
 from nectr.users.models import User
 
 
 class Tutor(models.Model):
     user = models.ForeignKey(User)
+    student = models.ForeignKey(Student)
