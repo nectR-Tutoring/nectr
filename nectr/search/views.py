46acdc34a47ae3c0a04a0330b629e0c0c0702ba6
Brandon Fox <bfox@foxtrot.email>
2017-04-28 02:16:37 -0400

    edited view

--- nectr/search/views.py
+++ nectr/search/views.py
@@ -4,5 +4,5 @@ from nectr.tutor.models import Tutor
 
 
 def search(request):
-    tutors = get_list_or_404(Tutor)
-    return render(request, 'search/tutor/tutor_search_result_list.html', tutors)
+    tutors = Tutor.objects.all()
+    return render(request, 'search/base_search.html', {'tutors': tutors})
