fb180c23f0aedcf95e6d9fc7c13790302273d5d4
Brandon Fox <bfox@foxtrot.email>
2017-04-28 02:38:21 -0400

    edited view

--- nectr/tutor/models.py
+++ nectr/tutor/models.py
@@ -5,10 +5,14 @@ from nectr.student.models import Student
 from nectr.users.models import User
 
 
-class Tutor(models.Model):
-    base_user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Tutor(User):
+    # Possibly define my own link back to parent if necessary
+    # user_ptr = models.OneToOneField(
+    #     User, on_delete=models.CASCADE,
+    #     parent_link=True,
+    # )
     votes = models.IntegerField(default=0)
     courses = models.ManyToManyField(Courses)
 
     def __str__(self):
-        return self.base_user.username
+        return self.username
