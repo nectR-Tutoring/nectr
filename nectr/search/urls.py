32a5d3b0867797784879926c2d6e1c9811ac2380
BrandonFox <bfox@foxtrot.email>
2017-05-01 17:56:09 -0400

    commented out urls.py

--- nectr/search/urls.py
+++ nectr/search/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
 from nectr.search.models import TutorList
-
-urlpatterns = [
-    url(
-        regex=r'^tutors/all'
-        view=TutorList.as_view()
-        name='list'
-    ),
-]
+#
+# urlpatterns = [
+#     url(
+#         regex=r'^tutors/all'
+#         view=TutorList.as_view()
+#         name='list'
+#     ),
+# ]
