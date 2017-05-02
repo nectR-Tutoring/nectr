ec7d5919b311000f6445e5690c04334f2acfd902
BrandonFox <bfox@foxtrot.email>
2017-05-02 09:34:25 -0400

    removed haystack and stripped down docker-compose for speed

--- config/settings/base.py
+++ config/settings/base.py
@@ -52,7 +52,7 @@ THIRD_PARTY_APPS = [
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
-    'haystack',  # search
+    # 'haystack',  # search
 ]
 
 # Apps specific for this project go here.
