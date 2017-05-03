eb597301d18dbde699531129ee85a47cb6e6c32f
BrandonFox <bfox@foxtrot.email>
2017-05-03 15:22:20 -0400

    working

--- nectr/dashboard/urls.py
+++ nectr/dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
+from nectr.dashboard import views
 from nectr.dashboard.views import DashboardView
 
 urlpatterns = [
@@ -9,4 +10,9 @@ urlpatterns = [
         view=login_required(DashboardView.as_view()),
         name='dashboard_view'
     ),
+    url(
+        regex=r'^student_dashboard',
+        view=login_required(views.student_dashboard),
+        name='student_dashboard'
+    )
 ]
--- nectr/dashboard/views.py
+++ nectr/dashboard/views.py
@@ -6,3 +6,11 @@ from django.views.generic import TemplateView
 
 class DashboardView(TemplateView):
     template_name = "dashboard/complete_profile.html"
+
+
+def student_dashboard(request):
+    objects = ""
+    objects.username = request.user.username
+    objects.transactions = ""
+
+    return render(request, 'dashboard/Student_Dashboard.html', {'objects': objects})
