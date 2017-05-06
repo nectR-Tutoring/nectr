c3cb69142b203ed0a4ef639adb1196023e4412aa
Brandon Fox <bfox@foxtrot.email>
2017-05-06 12:41:40 -0400

    add foreignkey for Tutor

--- nectr/search/views.py
+++ nectr/search/views.py
@@ -8,15 +8,12 @@ from nectr.tutor.models import Tutor
 class Search(View):
     model = Tutor
 
-    def get(self, request):
+    @staticmethod
+    def get(request):
         search = request.GET.get('search_text', '')
         if search is not '':
-            try:
-                tutors = Tutor.objects.get(courses__course_name__contains=search)
-                return render(request, 'search/search_with_results.html', {'tutors': tutors, 'search_text': search})
-            except ObjectDoesNotExist:
-                tutors = Tutor.objects.all()
-                return render(request, 'search/search_with_results.html', {'tutors': tutors, 'search_text': search})
+            tutors = Tutor.objects.get(courses__course_name__contains=search)
+            return render(request, 'search/search_with_results.html', {'tutors': tutors, 'search_text': search})
         else:
             tutors = Tutor.objects.all()
             return render(request, 'search/base_search.html', {'tutors': tutors})
--- nectr/tutor/models.py
+++ nectr/tutor/models.py
@@ -5,11 +5,6 @@ from nectr.users.models import User
 
 
 class Tutor(models.Model):
-    # Possibly define my own link back to parent if necessary
-    # user_ptr = models.OneToOneField(
-    #     User, on_delete=models.CASCADE,
-    #     parent_link=True,
-    # )
     user = models.ForeignKey(User, default=1, null=True)
     votes = models.IntegerField(default=0)
     courses = models.ManyToManyField(Courses)
