b7eaa1b67013741538f5b47556a705750a023e4c
Brandon Fox <bfox@foxtrot.email>
2017-05-06 12:25:02 -0400

    add foreignkey for Tutor

--- nectr/tutor/models.py
+++ nectr/tutor/models.py
@@ -4,14 +4,15 @@ from nectr.courses.models import Courses
 from nectr.users.models import User
 
 
-class Tutor(User):
+class Tutor(models.Model):
     # Possibly define my own link back to parent if necessary
     # user_ptr = models.OneToOneField(
     #     User, on_delete=models.CASCADE,
     #     parent_link=True,
     # )
+    user = models.ForeignKey(User, default=1, null=True)
     votes = models.IntegerField(default=0)
     courses = models.ManyToManyField(Courses)
 
     def __str__(self):
-        return self.username
+        return self.user.username
