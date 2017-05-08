96e7565da3009677d330a90a6a07b17a2d5de66d
Brandon Fox <bfox@foxtrot.email>
2017-05-08 00:46:48 -0400

    fixed search get

--- nectr/search/views.py
+++ nectr/search/views.py
@@ -12,7 +12,7 @@ class Search(View):
         search = request.GET.get('search_text', '')
         if search is not '':
             tutors = Tutor.objects.filter(courses__course_name__contains=search)
-            return render(request, 'search/search_with_results.html', {'tutors': tutors, 'search_text': search})
+            return render(request, 'search/base_search.html', {'tutors': tutors, 'search_text': search})
         else:
             tutors = Tutor.objects.all()
             return render(request, 'search/base_search.html', {'tutors': tutors})
