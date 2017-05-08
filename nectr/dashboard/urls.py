7daa9c98dfc583560c7c137f13e511d9b3ea5f88
Brandon Fox <bfox@foxtrot.email>
2017-05-08 00:39:16 -0400

    dashboard created

--- nectr/dashboard/urls.py
+++ nectr/dashboard/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from nectr.dashboard.views import DashboardView
+from nectr.dashboard import views
 
 urlpatterns = [
     url(
         regex=r'^$',
-        view=login_required(DashboardView.as_view()),
+        view=login_required(views.DashboardView.as_view()),
         name='dashboard_view'
+    ),
+    url(
+        regex=r'^edit_profile',
+        view=login_required(views.DashboardEditProfile.as_view()),
+        name='edit_profile'
     ),
 ]
--- nectr/dashboard/views.py
+++ nectr/dashboard/views.py
@@ -6,3 +6,9 @@ from django.views.generic import TemplateView
 
 class DashboardView(TemplateView):
     template_name = "dashboard/base_dashboard.html"
+
+
+class DashboardEditProfile(TemplateView):
+    template_name = "dashboard/edit_profile.html"
+
+
