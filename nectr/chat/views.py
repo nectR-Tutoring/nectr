b64ec2923da2eda93a8da39d1db712f5474fe5e5
Ilya Shupta <funn17@gmail.com>
2017-05-09 18:38:44 +0700

    Front-end skeleton for chat.

--- nectr/chat/views.py
+++ nectr/chat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class ChatView(TemplateView):
+    template_name = 'chat/base_chat.html'
