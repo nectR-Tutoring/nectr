aa153c21059c4d3ca469a1dd181fb074e63250ac
Ilya Shupta <funn17@gmail.com>
2017-05-09 22:00:22 +0700

    Echo server with basic chat front-end.

--- nectr/chat/views.py
+++ nectr/chat/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 from django.views.generic import TemplateView
 
+from nectr.users.models import User
+
 
 class ChatView(TemplateView):
     template_name = 'chat/base_chat.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ChatView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['user_list'] = User.objects.exclude(id=self.request.user.id)
+        return context
--- nectr/static/js/chat.js
+++ nectr/static/js/chat.js
@@ -1,13 +1,39 @@
 (function($) {
     $(function() {
-        var ChatSocket = function() {
-            var uri = (window.location.protocol == 'https:' ? 'wss' : 'ws') + '://' + window.location.host + '/ws-chat/';
+        var ChatView = function(root) {
             return {
-                listenMap: {
-                    message: function(payload) {
-                        alert(payload);
-                    }
+                createMessageElement: function(avatar, username, text, time) {
+                    return '<div class="message"><img class="message-avatar" src="' + avatar + '"><span class="message-username">' + username + '</span>' +
+                        '<p class="message-text">' + text + '</p></div>' +
+                        '<span class="message-time">' + time + '</span>';
                 },
+                onMessage: function(payload) {
+                    var message = this.createMessageElement('/static/img/faces/face-3.jpg', payload.user, payload.text, payload.time);
+                    $(root).append(message);
+                },
+                onInit: function(payload) {
+                    $(root).empty();
+                    payload.messages.forEach(this.onMessage);
+                }
+            };
+        };
+
+        var UserView = function(root) {
+            return {
+                init: function() {
+                    $(root).children().each(function() {
+                        $(this).click(function(event) {
+                            window.socket.emit('init', event.target.id.substring(1));
+                        });
+                    });
+                }
+            }
+        };
+
+        var ChatSocket = function() {
+            var uri = (window.location.protocol === 'https:' ? 'wss' : 'ws') + '://' + window.location.host + '/ws-chat/';
+            return {
+                listenMap: {},
                 emit: function(type, payload) {
                     if (this.websocket.readyState > 1) {
                         this.websocket.onerror(null);
@@ -50,9 +76,29 @@
                         }, time);
                     }
                 }
-            }
+            };
         };
         window.socket = new ChatSocket();
         window.socket.createWebSocket();
+
+        window.chatview = new ChatView($('#message-list')[0]);
+        window.socket.on('message', window.chatview.onMessage.bind(window.chatview));
+        window.socket.on('init', window.chatview.onInit.bind(window.chatview));
+
+        window.userview = new UserView($('#user-list')[0]);
+        window.userview.init();
+
+        $('#message-form').submit(function() {
+            var text = $('#message-input').val();
+            if (text.length > 0) {
+                window.socket.emit('message', {
+                    user: 'John',
+                    text: text,
+                    time: '12:34'
+                });
+            }
+            $('#message-input').val('');
+            $('#message-input').focus();
+        });
     });
 })(jQuery);
