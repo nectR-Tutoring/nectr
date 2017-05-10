3aab968ea412c11d47db14200fb29694f7e2b567
Ilya Shupta <funn17@gmail.com>
2017-05-10 12:41:36 +0700

    Chat models.

--- nectr/chat/models.py
+++ nectr/chat/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+from nectr.users.models import User
+
+
+class Conversation(models.Model):
+    initiator = models.ForeignKey(User, related_name='+')  # TODO: Refactor this into ManyToMany when chat groups will be implemented.
+    recipient = models.ForeignKey(User, related_name='+')
+
 
 class Message(models.Model):
-    message = models.CharField(max_length=10)
+    text = models.CharField(max_length=1024)
+    author = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    conversation = models.ForeignKey(Conversation)
 
     def __str__(self):
-        return self.message
+        return '{}: {}'.format(self.author, self.text)
