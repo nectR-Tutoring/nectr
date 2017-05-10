9a9187e4404f7ca9ea624e9743f9f1bd83a263b7
BrandonFox <bfox@foxtrot.email>
2017-05-10 15:38:02 -0400

    added catch for POST

--- nectr/dashboard/views.py
+++ nectr/dashboard/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 
+from nectr.courses.models import Courses
+
 
 class DashboardView(TemplateView):
     template_name = "dashboard/base_dashboard.html"
