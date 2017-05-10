cc0c14335392665b029ad12ea915ae6c41ad7341
Charlie Valerio <valec4@farmingdale.edu>
2017-05-10 16:39:36 -0400

    not working, edit doesnt show up

--- nectr/dashboard/views.py
+++ nectr/dashboard/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from nectr.courses.models import Courses
+from nectr.users.models import User
 
 
 class DashboardView(TemplateView):
@@ -18,7 +19,10 @@ class DashboardEditCourses(TemplateView):
     template_name = "dashboard/edit_courses.html"
 
     def post(self, request):
-        return render(request, 'dashboard/edit_courses.html')
+        user = User.objects.get(username=request.user.username)
+        user.courses.subject = request.POST.get('subject')
+        user.courses.course_name = request.POST.get('course')
+        return render(request, 'dashboard/edit_courses.html', {'courses' : user.courses})
 
 
 class DashboardEditSkills(TemplateView):
