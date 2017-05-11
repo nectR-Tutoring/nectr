fccd6450d4f50b3ee5219baf744f3f2d9c0a65c6
Brandon Fox <bfox@foxtrot.email>
2017-05-11 00:26:06 -0400

    edit dashboard

---
+++ nectr/dashboard/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class CoursesForm(forms.Form):
+    #  <input name="subject" type="text" id="subject" placeholder="Subject">
+    #                 <input name="course" type="text" id="course" placeholder="Courses">
+    subject = forms.CharField()
+    course = forms.CharField()
