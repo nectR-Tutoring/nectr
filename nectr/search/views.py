732d8086da681863b0362448fdb299d37295f0ce
BrandonFox <bfox@foxtrot.email>
2017-05-15 14:20:54 -0400

    Added users name to search

--- nectr/search/views.py
+++ nectr/search/views.py
@@ -14,8 +14,9 @@ class Search(View):
         if search is not '':
             users_courses = User.objects.filter(courses__course_name__contains=search)
             users_skills = User.objects.filter(skills__skill__contains=search)
+            users_name = User.objects.filter(name__contains=search)
             from itertools import chain
-            users = list(chain(users_courses, users_skills))
+            users = list(chain(users_courses, users_skills, users_name))
         else:
             users = User.objects.all()
         return render(request, 'search/base_search.html', {'tutors': users, 'search_text': search})
