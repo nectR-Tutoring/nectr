a361b4d7da07811d7edee86080cd39b2a9da6d74
Charlie Valerio <valec4@farmingdale.edu>
2017-05-16 15:59:11 -0400

    subject dropdowns

--- nectr/search/views.py
+++ nectr/search/views.py
@@ -11,12 +11,14 @@ class Search(View):
     @staticmethod
     def get(request):
         search = request.GET.get('search_text', '')
+        subject = request.GET.get('subject')
         if search is not '':
             users_courses = User.objects.filter(courses__course_name__contains=search)
+            users_subject = User.objects.filter(courses__subject__contains=subject)
             users_skills = User.objects.filter(skills__skill__contains=search)
             users_name = User.objects.filter(name__contains=search)
             from itertools import chain
-            users = list(chain(users_courses, users_skills, users_name))
+            users = list(chain(users_courses, users_skills, users_name, users_subject))
         else:
             users = User.objects.all()
         return render(request, 'search/search_with_results.html', {'tutors': users, 'search_text': search})
